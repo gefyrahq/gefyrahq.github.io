5be21432eada96dd420abdfe5981054386d4642d
Robert Stein <robert@blueshoe.io>
2023-07-17 12:10:24 +0200

    fix: redirects

--- gefyra/docusaurus.config.js
+++ gefyra/docusaurus.config.js
@@ -162,16 +162,6 @@ const config = {
           },
 
         ],
-        createRedirects(existingPath) {
-          if (existingPath.includes('/community')) {
-            // Redirect from /docs/team/X to /community/X and /docs/support/X to /community/X
-            return [
-              existingPath.replace('/community', '/docs/team'),
-              existingPath.replace('/community', '/docs/support'),
-            ];
-          }
-          return undefined; // Return a falsy value: no redirect created
-        },
       },
     ],
   ],
